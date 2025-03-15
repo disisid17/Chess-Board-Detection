3579e8b2283238464a66583d834129314de1d1a1
disisid17 <sidharthsandeep17@gmail.com>
2025-03-14 21:50:10 -0400

    pi day final

--- cropr.py
+++ cropr.py
@@ -1,13 +1,4 @@
 import cv2
-
-def crop_image(image):
-    # Placeholder function to crop the image
-    pass
-
-def resize_image(image):
-    # Placeholder function to resize the image
-    pass
-
 def main():
     cap = cv2.VideoCapture(0)
 
@@ -20,11 +11,8 @@ def main():
         if not ret:
             print("Error: Failed to capture image.")
             break
-        frame = frame[145:930,595:1380]
+        frame = frame[140:940,590:1390]
         # Call the crop and resize functions (currently do nothing)
-        cropped_frame = crop_image(frame)
-        
-
         cv2.imshow('Live Video Stream', frame)
 
         if cv2.waitKey(1) & 0xFF == ord('q'):
@@ -32,6 +20,5 @@ def main():
 
     cap.release()
     cv2.destroyAllWindows()
-
 if __name__ == "__main__":
     main()
--- readboard.py
+++ readboard.py
@@ -1,2 +1,21 @@
 import cv2
 import numpy as np
+
+cap = cv2.VideoCapture(0)
+while True:
+        ret, frame = cap.read()
+        if not ret:
+            print("Error: Failed to capture image.")
+            break
+        frame = frame[145:930,595:1380]
+        # Call the crop and resize functions (currently do nothing)
+        cropped_frame = crop_image(frame)
+        
+
+        cv2.imshow('Live Video Stream', frame)
+
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+cap.release()
+cv2.destroyAllWindows()
